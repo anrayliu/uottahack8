2ec230210a04826648cac69cec6e8f82674913f8
Anray Liu <anrayliu@gmail.com>
2026-01-17 11:46:55 -0500

    feat: set up and serve react

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    static_folder="frontend/build",
+)
 
 @app.route("/")
 def index():
-    return "hello world"
+    return send_from_directory(app.static_folder, "index.html")
+
+@app.route("/<path:path>")
+def static_files(path):
+    return send_from_directory(app.static_folder, path)
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
